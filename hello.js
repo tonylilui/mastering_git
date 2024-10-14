10728c8fcd26a341429b04af2eb9d8d8307181ab
Tony Li <tony.lilui416@gmail.com>
2024-10-13 20:12:16 -0400

    stash conflict solved

--- hello.js
+++ hello.js
@@ -6,3 +6,11 @@ console.log("stash");
 
 console.log("stash");
 console.log("stash");
+
+console.log("Hello github");
+console.log("Hello from dev-tony");
+
+//Im in the process of writing important code
+//code
+//code
+//code
